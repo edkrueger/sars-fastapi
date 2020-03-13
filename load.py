928032ebb205d72de632bd066aae921d6512a258
Edward Krueger <edkrueger@gmail.com>
2020-03-13 11:42:26 -0500

    Load the table correctly. Create all tables in the load script.

--- load.py
+++ load.py
@@ -2,9 +2,11 @@ import csv
 import datetime
 
 from app import models
-from app.database import SessionLocal
+from app.database import SessionLocal, engine
 
 db = SessionLocal()
+
+models.Base.metadata.create_all(bind=engine)
 
 with open("sars_2003_complete_dataset_clean.csv", "r") as f:
     csv_reader = csv.DictReader(f)
@@ -14,7 +16,7 @@ with open("sars_2003_complete_dataset_clean.csv", "r") as f:
             date=datetime.datetime.strptime(row["date"], "%Y-%m-%d"),
             country=row["country"],
             cases=row["cases"],
-            deaths=row["cases"],
+            deaths=row["deaths"],
             recoveries=row["recoveries"],
         )
         db.add(db_record)
