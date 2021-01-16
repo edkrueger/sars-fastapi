2160bcabc432d4434f1f249bdd4805d16f9fa6fc
fliot <francois@liot.org>
2021-01-16 17:28:03 +0100

    logger + async scheduler

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from typing import List
-
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
+from typing import List
+
+import pytz
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from fastapi.logger import logger
+import logging
+gunicorn_logger = logging.getLogger('gunicorn.error')
+logger.handlers = gunicorn_logger.handlers
+if __name__ != "main":
+    logger.setLevel(gunicorn_logger.level)
+else:
+    logger.setLevel(logging.DEBUG)
 
 from . import models, schemas
 from .database import SessionLocal, engine
@@ -29,6 +40,19 @@ def get_db():
     finally:
         db.close()
 
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Startup event !")
+
+    scheduler = AsyncIOScheduler(timezone=pytz.utc)
+    scheduler.start()
+    scheduler.add_job(count_records_task, "cron", minute='*')  # runs every minute
+
+def count_records_task():
+    db = SessionLocal()
+    n = db.query(models.Record).count()
+    logger.info(f"Cron counter {n}")
+
 
 @app.get("/")
 def main():
@@ -37,5 +61,6 @@ def main():
 
 @app.get("/records/", response_model=List[schemas.Record])
 def show_records(db: Session = Depends(get_db)):
+    logger.info("Rest call")
     records = db.query(models.Record).limit(300).all()
     return records
