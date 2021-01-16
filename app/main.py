1107c06c341f8e823764542d030c1d0dcee04c28
fliot <francois@liot.org>
2021-01-16 22:08:15 +0100

    get/post/put/delete complete examples + key autoincrement

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
@@ -59,8 +59,41 @@ def main():
     return RedirectResponse(url="/docs/")
 
 
-@app.get("/records/", response_model=List[schemas.Record])
-def show_records(db: Session = Depends(get_db)):
-    logger.info("Rest call")
+# get all
+@app.get("/records/", summary="Get 300 records", response_model=List[schemas.Record])
+def get_all_records(db: Session = Depends(get_db)):
     records = db.query(models.Record).limit(300).all()
     return records
+
+# create new
+@app.post("/records/", summary="Post new record", response_model=schemas.Record)
+def post_record(rec: schemas.RecordNew, db: Session = Depends(get_db)):
+    db_record = models.Record(**rec.dict())
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
+    return db_record
+
+# get single
+@app.get("/records/{rec_id}", summary="Get a record", response_model=schemas.Record)
+def get_record(rec_id: int, db: Session = Depends(get_db)):
+    record = db.query(models.Record).filter(models.Record.id == rec_id).first()
+    return record
+
+# modify existing
+@app.put("/records/{rec_id}", summary="Modify a record", response_model=schemas.Record)
+def put_record(rec_id: int, rec: schemas.RecordNew, db: Session = Depends(get_db)):
+    db.query(models.Record).filter(models.Record.id == rec_id).update(rec.dict())
+    db.commit()
+    db_record = db.query(models.Record).filter(models.Record.id == rec_id).first()
+    return db_record
+
+# delete existing
+@app.delete("/records/{rec_id}", summary="Delete a record", response_class=Response, responses={200: {"description": "Record successfully deleted"},404: {"description": "Record not found"},},)
+def delete_record(rec_id: int, db: Session = Depends(get_db)):
+    if db.query(models.Record).filter(models.Record.id == rec_id).count() == 0:
+        return Response(status_code=404)
+    else:
+        db.query(models.Record).filter(models.Record.id == rec_id).delete()
+        db.commit()
+        return Response(status_code=200)
